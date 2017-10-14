0269222c2fa536841cb2a57a1edda043ce5bcfae
KaiB-P <kbernardpearl@gmail.com>
2017-10-14 12:05:08 -0700

    second draft

--- Kai Bernard-Pearl's Things/Attack.py
+++ Kai Bernard-Pearl's Things/Attack.py
@@ -1,26 +1,35 @@
 import random
-class blob:
-	atk = 10
-	dfnc = 10
-	atkpow = 1
-	hp = 50
-class character:
-	atk1 = 10
-	atk2 = 15
-	atk3 = random.randint(5,50)
-	dfnc = 20
-	atkpow = 5
-	hp = 100
+class Blob:
+	def __init__(self):
+		self.atk = 10
+		self.dfnc = 10
+		self.atkpow = 1
+		self.hp = 50
+
+class Character:
+	def __init__(self, atk,limit):
+		self.atk = atk
+		self.dfnc = 20
+		self.atkpow = 5
+		self.hp = 100
+		self.limit = limit
 
 
-choice = input("Choose your attack:\nA) Sash\nB) Stab\nC) Pummel")
+choice = input("Choose your attack:\nA) Slash\nB) Stab\nC) Pummel")
 if choice == "a" or choice == "A":
-		atkchoice = character(atk1)
-elif choice == "b" or choice == "B":
-		atkchoice = character(atk2)
-elif choice == "c" or choice == "C":
-		atkchoice = character(atk3)
+		atkchoice = Character(10,20)
 
-def fightfomula (x,y,z):
-	return x.hp - y.atkpow*z.atk//y.dfnc,y.atkpow*z.atk//y.dfnc
-print("Character dealt "+str(fightfomula(blob,character,atkchoice)[1])+" hp. Blob is now at "+str(fightfomula(blob,character)[0])+" hp")
+elif choice == "b" or choice == "B":
+		atkchoice = Character(15,10)
+elif choice == "c" or choice == "C":
+		atkchoice = Character(random.randint(5,50),5)
+
+blob = Blob()
+
+def fightfomula (x,y):
+	return x.hp - y.atkpow*y.atk//x.dfnc,y.atkpow*y.atk//y.dfnc
+print("Character dealt "+str(fightfomula(blob,atkchoice)[1])+" hp. Blob is now at "+str(fightfomula(blob,atkchoice)[0])+" hp")
+
+
+
+ def atklimit(x):
